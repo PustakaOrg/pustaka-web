c6bfa17b933fdc6196e6c0de46528994d4d540f7
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-04-06 13:45:00 +0800

    add theme provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,13 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import { queryClient } from "../shared/utils/queryClient";
+import { ThemeProvider } from "~/shared/components/theme";
 
 const Providers = ({ children }: React.PropsWithChildren) => {
 	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider>{children}</ThemeProvider>
+		</QueryClientProvider>
 	);
 };
 
