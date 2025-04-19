6e0ab5f3ac2ba899fb5f696bc0641e0233af8d0a
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-04-19 20:33:11 +0800

    fix weird layout thing

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Button } from "./ui/button";
 const Navbar = () => {
 	return (
 		<header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4">
-			<div className="container flex h-16 items-center justify-between">
+			<div className="flex h-16 items-center justify-between">
 				<div className="flex items-center gap-2">
 					<BookOpen className="h-6 w-6" />
 					<span className="text-xl font-bold">Pustaka</span>
