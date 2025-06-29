9fd735d503619d00bdf0ec1efca33f01271035b8
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-06-29 21:57:33 +0800

    minor change

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { GalleryVerticalEnd } from "lucide-react";
 import { useEffect } from "react";
-import { useNavigate } from "react-router";
+import { Link, useNavigate } from "react-router";
 import LoginForm from "~/features/auth/components/LoginForm";
 import useProfile from "~/features/auth/hooks/useProfile";
 
@@ -8,7 +8,6 @@ const LoginPage = () => {
 	const navigate = useNavigate();
 	const { profile } = useProfile();
 	useEffect(() => {
-    console.log("Profile", profile)
 		if (profile !== undefined) {
 			navigate("/dashboard");
 		}
@@ -17,12 +16,12 @@ const LoginPage = () => {
 		<div className="grid min-h-svh lg:grid-cols-2">
 			<div className="flex flex-col gap-4 p-6 md:p-10">
 				<div className="flex justify-center gap-2 md:justify-start">
-					<a href="#" className="flex items-center gap-2 font-medium">
+					<Link to="/" className="flex items-center gap-2 font-medium">
 						<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
 							<GalleryVerticalEnd className="size-4" />
 						</div>
 						Pustaka.
-					</a>
+					</Link>
 				</div>
 				<div className="flex flex-1  items-center justify-center">
 					<div className="w-full max-w-sm -mt-24">
@@ -30,12 +29,10 @@ const LoginPage = () => {
 					</div>
 				</div>
 			</div>
-			<div className="relative hidden bg-muted lg:block">
-				<img
-					src="/placeholder.svg"
-					alt="Image"
-					className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-				/>
+			<div className="relative hidden lg:flex justify-center items-center p-4">
+				<div className="size-full bg-primary rounded-lg flex p-4">
+					<div className="mt-auto text-primary-foreground font-semibold">Pustaka.</div>
+				</div>
 			</div>
 		</div>
 	);
