4794634b34af8a4b14801f6a87267b74fac0e863
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2025-06-30 17:53:48 +0800

    local https

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import fs from "fs"
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	server: {
+		https: {
+			key: fs.readFileSync(path.resolve(__dirname, "cert/pc.local-key.pem")),
+			cert: fs.readFileSync(path.resolve(__dirname, "cert/pc.local.pem")),
+		},
+		allowedHosts: ["pc.local"],
+		host: "pc.local",
+		port: 5173,
+	},
 	resolve: {
 		alias: {
 			"~": path.resolve(__dirname, "./src"),
